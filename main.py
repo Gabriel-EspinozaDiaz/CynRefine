843d5155858bf4cd16351d4b029f67822c1e8d1a
Gabriel-EspinozaDiaz <gusmxno@gmail.com>
2023-07-11 16:47:59 +0100

    setup2

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from bs4 import BeautifulSoup
-
 import requests as req
 
 page = req.get('http://parts.igem.org/Part:BBa_K3237027')
-
 soup = BeautifulSoup(page.text,'html.parser')
-
 print(soup.prettify())
