c84fb0ad65f9078e4890889ba9d5c1db6f072032
Gabriel-EspinozaDiaz <gusmxno@gmail.com>
2023-07-11 16:47:52 +0100

    setup

---
+++ main.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+import requests as req
+
+page = req.get('http://parts.igem.org/Part:BBa_K3237027')
+
+soup = BeautifulSoup(page.text,'html.parser')
+
+print(soup.prettify())
