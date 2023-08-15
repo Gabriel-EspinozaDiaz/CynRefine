54f0f4b4e30c50ec2b1e421fe384a7b5bb3e2f83
Gabriel-EspinozaDiaz <gusmxno@gmail.com>
2023-08-15 13:49:19 +0100

    feature: part status straping

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import os
 '''
 
 scrape1 = scraping.Scraper('http://parts.igem.org/Part:BBa_C0040')
+scrape2 = scraping.Scraper('http://parts.igem.org/wiki/index.php?title=Part:BBa_K2607001')
 
 scrape1.get_content()
-print(scrape1.check_fasta())
+print(scrape1.content_get_data('ex1'))
+scrape2.get_content()
+print(scrape2.content_get_data('ex2'))
--- scraping.py
+++ scraping.py
@@ -45,10 +45,52 @@ class Scraper:
         '''
         return self.content.prettify()
     
-    def content_get_text(self):
-
-        return self.content.get_text()
+    def content_get_data(self,name):
+        '''
+        Once this method is finished, it will break down a page by 
+        '''
+        end = 'Part:BBa_'
+        basic = self.content.get_text()
+        basic = basic[basic.find('main page'):]
+        prelude = basic[:basic.find(end)]
+        # Determines all tag data from status box information (see info on status boxes at http://parts.igem.org/Help:Part_Status_Box)
+        part_status = self.get_part_status(prelude)
+        #sample_status = self.get_sample_status(prelude)
+        #experience = self.get_experience(prelude)
+        #uses = self.get_uses(prelude)
+        #twins = self.get_twins(prelude)
     
+        with open(name+'.txt', 'w') as f:
+            f.write(basic)
+        return part_status
+    
+    def get_part_status(self,text):
+        start1_2 = 'Released' #This counts for Released or Released HQ entries
+        start3 = 'Not Released'
+        start4 = 'Discontinued'
+        if start4 in text:
+            status = start4
+        elif start3 in text:
+            status = start3
+        elif start1_2 in text:
+            status = start1_2
+        else:
+            print('Error occured: no release status found')
+        return status
+    
+    def get_sample_status(self,text):
+        return 0
+    
+    def get_experience(self,text):
+        return 0
+    
+    def get_uses(self,text):
+        return 0
+    
+    def get_twins(self,text):
+        return 0
+
+
     def check_fasta(self):
         '''
         Every page with an attached sequence has a counterpart page under the format "http://parts.igem.org/fasta/parts/BBa_xxxxx"
