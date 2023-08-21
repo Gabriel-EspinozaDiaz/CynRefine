bab157af5dfb834f6a4b766fb79e43232daa17d1
Gabriel-EspinozaDiaz <gusmxno@gmail.com>
2023-08-21 12:35:01 +0100

    feature: get size of multiple page csvs

--- iterations.py
+++ iterations.py
@@ -3,6 +3,7 @@ import os
 import sys
 import time
 import csv
+import numpy as np
 '''
 
 '''
@@ -28,10 +29,15 @@ class Iterator:
         Scrapes all web contents used by 
         '''
         total = 0
+        urls = []
+        #Read all urls into a list
         file = open(url_list,'r')
-        
-        os.remove("demofile.txt")
-        print(total)
+        for n in file.readlines():
+            urls.append(n)
+        file.close()
+        #Write each
+
+
         file.close()
 
 class Trial:
@@ -40,24 +46,44 @@ class Trial:
     This is just for testing on the scope of a single file using a similar format to that of the iterator without the risk of blowing up my laptop. 
     '''
 
-    def page_as_csv(url,name,scrape):
+    def page_as_csv(self,name,scrape):
         with open(name+'.csv', 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(['part','part_status','sample_status','experience','uses','twins','authors','team','date','text'])
             csv.writer(file).writerow(scrape.package_for_csv())
     
-    def check_size(url,scrape):
+    def check_size(self,scrape):
         with open('temp_file.csv','w',newline='') as file:
             writer = csv.writer(file)
             writer.writerow(['part','part_status','sample_status','experience','uses','twins','authors','team','date','text'])
             csv.writer(file).writerow(scrape.package_for_csv())
-        print('\nTEMPORARY FILE INITIALISED')
+        print('\nTEMPORARY FILE INITIALISED\n')
         size = os.path.getsize('temp_file.csv')
         print(f'filesize: {size} bytes')
-        time.sleep(5)
         os.remove('temp_file.csv')
         print('\nTEMPORARY FILE DELETED')
 
+
+    def dual_check_size(self,url_list):
+        total = 0
+        urls = []
+        #Store urls in list
+        file = open(url_list,'r')
+        for n in file.readlines():
+            urls.append(n.replace('\n',''))
+        file.close()
+        #Loop through 
+        for n in range(len(urls)):
+            with open('temp_file.csv','w',newline='') as file:
+                url = urls[n]
+                scrape = Scraper(url)
+                csv.writer(file).writerow(scrape.package_for_csv())
+                time.sleep(1)
+            print('\nTEMPORARY FILE INITIALISED\n')
+            total += os.path.getsize('temp_file.csv')
+            print(f'current repository size: {total} bytes')
+            os.remove('temp_file.csv')
+            print('\nTEMPORARY FILE DELETED')
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from scraping import Scraper
 import iterations
-import os
+import time
 
 '''
 
@@ -10,5 +10,14 @@ scrape1 = Scraper('http://parts.igem.org/Part:BBa_C0040')
 scrape2 = Scraper('http://parts.igem.org/wiki/index.php?title=Part:BBa_K2607001')
 trial = iterations.Trial()
 
-trial.page_as_csv('example1',scrape1)
-trial.page_as_csv('example2',scrape2)
+trial.page_as_csv('ex1',scrape1)
+time.sleep(1)
+trial.page_as_csv('ex2',scrape2)
+time.sleep(1)
+trial.check_size(scrape1)
+time.sleep(1)
+trial.check_size(scrape2)
+time.sleep(1)
+
+trial.dual_check_size('shortlist.txt')
+
