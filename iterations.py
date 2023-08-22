4b47bb65e5bbdf58cd3d4f555400dfa60ce59df7
Gabriel-EspinozaDiaz <gusmxno@gmail.com>
2023-08-22 11:42:44 +0100

    fix: method division (Iterator)

--- iterations.py
+++ iterations.py
@@ -3,49 +3,38 @@ import os
 import sys
 import time
 import csv
-import numpy as np
 '''
 
-'''
-
-
+'''  
 
 class Iterator:
     def __init__(self):
         self.errors = []
 
-
-    def get_size_url(url_list):
+    def get_urls(url_list):
         '''
-        Takes a list of urls and scrapes each one, determining required to store all of their data
+        Takes a txt file containing all urls and loads them all into a list
         '''
         urls = []
-        file = open(url_list,'r')
-        for n in file.readlines():
-            urls.append(n)
-        print(urls)
-        print(sys.getsizeof(urls))
-        file.close()
-
-    def get_size_csvs(self,url_list,sPoint=1,ePoint=-1):
-        '''
-        Takes an starting point (sPoint, int), ending point (ePoint, int) and txt file name as parameters (str)
-        Checks all urls listed in the file, writing all content into a csv file and then 
-        Based on the current size of the repository and the need for breaks to prevent getting kicked off the website, this method takes 6-8 hours to complete
-        On account of that, sPoint can be used to start the process at a desired file. 
-        The method prints the name and line number of the url in url_list. If you want to skip over files, use the last printed file # as sPoint 
-        '''
-        total = 0
-        urls = []
-        #Store urls in list
         file = open(url_list,'r')
         for n in file.readlines():
             urls.append(n.replace('\n',''))
         file.close()
+        return urls
+
+    def get_size_csvs(self,url_list,sPoint=1,ePoint=-1):
+        '''
+        Takes an starting point (sPoint, int), ending point (ePoint, int) and txt file name as parameters (str)
+        Checks all urls listed in the file, writing all content into a csv file, determining and recording the size of that file, and then deleting
+        Based on the current size of the repository and the need for breaks to prevent getting kicked off the website, this method takes 10-14 hours to complete
+        On account of that, sPoint can be used to start the process at a desired file. 
+        The method prints the name and line number of the url in url_list. If you want to skip over files, use the last printed file # as sPoint 
+        '''
+        total = 0
+        urls = self.get_urls(url_list)
         #check ePoint
         if ePoint < 0 or ePoint > len(urls):
             ePoint=len(urls)
-            print('ePoint defaulted')
         #Loop through 
         for n in range(sPoint-1,ePoint):
             try:
@@ -62,12 +51,32 @@ class Iterator:
                 self.errors.append(urls[n])
                 continue
             #Give the website a bit of time in between requests to avoid being blocked
-            time.sleep(1)
+            time.sleep(0.5)
+        print(f'\nFINAL SIZE REPOSITORY SIZE: {total}\n')
         with open('readme.txt', 'w') as file:
             for n in range(len(self.errors)):
                 file.write(self.errors[n]+'\n')
         
-        
+    def scrape_all(self,url_list,sPoint=1,ePoint=-1):
+        '''
+        Takes an starting point (sPoint, int), ending point (ePoint, int) and txt file name as parameters (str)
+        '''
+        urls = self.get_urls(url_list)
+        #check ePoint
+        if ePoint < 0 or ePoint > len(urls):
+            ePoint=len(urls)
+        #Loop through 
+        for n in range(sPoint-1,ePoint):
+            try:
+                scrape = Scraper(urls[n])
+            except:
+                #Record any errors that the scraper ran into
+                print(f'error occurred at file {n+1} ({urls[n]})')
+                self.errors.append(urls[n])
+                continue
+            #Give the website a bit of time in between requests to avoid being blocked
+            time.sleep(0.5)
+
 
 class Trial:
     '''
@@ -93,6 +102,9 @@ class Trial:
         print('\nTEMPORARY FILE DELETED')
 
     def dual_check_size(self,url_list):
+        '''
+        Testing for checking the size of multiple files
+        '''
         total = 0
         urls = []
         #Store urls in list
--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from scraping import Scraper
 import iterations
-import time
 
 '''
 
 '''
 
-#scrape1 = Scraper('http://parts.igem.org/Part:BBa_C0040') EXAMPLE
-#scrape2 = Scraper('http://parts.igem.org/wiki/index.php?title=Part:BBa_K2607001') EXAMPLE
+scrape1 = Scraper('http://parts.igem.org/Part:BBa_C0040')
+scrape2 = Scraper('http://parts.igem.org/wiki/index.php?title=Part:BBa_K2607001')
 #scrape3 = Scraper('http://parts.igem.org/Part:BBa_B1002') FIRST OCCURENCE OF RFC, CAUSED ERROR AT REGISTRY STAR SCRAPE
 #scrape4 = Scraper('http://parts.igem.org/Part:BBa_B1102') FIRST OCCURENCE OF 'NO RESULTS' REPSONSE, CAUSED ERROR AT SAMPLE EXPERIENCE SCRAPE
-scrape5 = Scraper('http://parts.igem.org/Part:BBa_C0012')
+#scrape5 = Scraper('http://parts.igem.org/Part:BBa_C0012')
 iterator = iterations.Iterator()
+trialrun = iterations.Trial()
 
-scrape5.write_content('spadoof')
-
-#iterator.get_size_csvs('allurls.txt',73)
+iterator.get_size_csvs('allurls.txt')
 
